e285e15b2588c51c618844ff884050f7c1e42a3b
Jared Grove <jaredkgrove@gmail.com>
2019-03-01 06:12:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
-  
-  string.split(" ").collect do |word|
+  word_array = string.split(" ")
+  word_array.collect do |word|
     word.reverse
   end
-  
+  word_array.join
 end
